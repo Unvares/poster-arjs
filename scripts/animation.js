9b683028c8788960763c3153feb03a6c643d874c
Rihards <okmanis8@gmail.com>
2021-10-26 17:40:26 +0500

    feat: created content rotation animation

--- scripts/animation.js
+++ scripts/animation.js
@@ -20,4 +20,21 @@ function startAnimation() {
         _AR.contentElement.setAttribute('animation',
           `property: scale; to: 1 1 1; dur: ${decreasingDuration}; easing: easeOutCubic;`);
     }, increasingDuration);
+
+    requestAnimationFrame(rotate);
+}
+
+function rotate() {
+  const rotationSpeed = 0.5;
+  const contentRotation = _AR.contentElement.getAttribute('rotation');
+  _AR.contentElement.setAttribute('rotation',
+    `${contentRotation.x} ${contentRotation.y} ${contentRotation.z + rotationSpeed}`);
+  
+    [..._AR.contentElement.children].forEach(el => {
+      const elementRotation = el.getAttribute('rotation');
+      el.setAttribute('rotation', 
+        `${elementRotation.x} ${elementRotation.y} ${elementRotation.z - rotationSpeed}`);
+    });
+  
+    requestAnimationFrame(rotate);
 }
